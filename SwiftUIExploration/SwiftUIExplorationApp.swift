d35b140ef7f6d5c9b4abbfdd6b72af5d0d23e056
Merlin <merlinzhao@gmail.com>
2021-01-24 21:06:20 -0500

    first commit

---
+++ SwiftUIExploration/SwiftUIExplorationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExplorationApp.swift
+//  SwiftUIExploration
+//
+//  Created by Merlin Zhao on 1/24/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExplorationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
