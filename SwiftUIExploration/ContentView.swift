598b0ebe318f857be71eb756463ab11698d1fa40
Merlin <merlinzhao@gmail.com>
2021-01-24 22:06:26 -0500

    ...

--- SwiftUIExploration/ContentView.swift
+++ SwiftUIExploration/ContentView.swift
@@ -7,15 +7,42 @@
 
 import SwiftUI
 
+
+// https://www.youtube.com/watch?v=IIDiqgdn2yo
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            MapView().frame(height: 300)
+                .ignoresSafeArea( edges: .top)
+            CircleImage().offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.black)
+                
+                HStack {
+                    Text("Joshua Tree National Park").font(.subheadline)
+                    Spacer()
+                    Text("California").font(.subheadline)
+                }
+                
+                Divider()
+
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+            }.padding()
+            Spacer()
+        }
     }
 }
-
+#if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+#endif
---
+++ SwiftUIExploration/MapView.swift
@@ -0,0 +1,30 @@
+//
+//  MapView.swift
+//  SwiftUIExploration
+//
+//  Created by Merlin Zhao on 1/24/21.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView()
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        //Code to update UIKit view
+        let coordinate = CLLocationCoordinate2D(latitude: 34.011286, longitude: -116.166868)
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        
+        uiView.setRegion(region, animated: true)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
